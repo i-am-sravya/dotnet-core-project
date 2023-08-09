30d54979a27dd55bfe2b6fd9776083ff1854d57a
Sravya Yakkati <ABDNXZZ@mmm.com>
2023-08-09 16:48:44 +0530

    Add project files.

---
+++ dotnet_project/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using dotnet_project.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly UserContext _context;
+
+        public UserController(IConfiguration config, UserContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult Create(User user)
+        {
+            if (_context.Users.Where(u => u.Email == user.Email).FirstOrDefault() != null)
+            {
+                return Ok("Already Exist");
+            }
+            user.MemberSince = DateTime.Now;
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok("Success");
+        }
+
+    }
+}
